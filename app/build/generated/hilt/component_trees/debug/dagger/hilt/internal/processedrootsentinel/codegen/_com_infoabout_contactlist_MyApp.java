0882e596a69feaaa7ff6a14286a57aeede7d19f1
user <monsterkil61@gmail.com>
2023-04-26 12:23:49 +0300

    clean project

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_infoabout_contactlist_MyApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.infoabout.contactlist.MyApp"
-)
-public final class _com_infoabout_contactlist_MyApp {
-}
